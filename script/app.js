1f7dbb5b1a718ae6940bdd6d08b0ac1e4a4971f2
Shahbano Siddique <malikaiza685@gmail.com>
2023-04-07 08:24:03 +0500

    FEAT

---
+++ script/app.js
@@ -0,0 +1,55 @@
+// Date 5.4.2023
+//  alert('hello js')
+// var user = prompt("Your Name PLease", "Usama")
+// alert(user)
+
+// var arr = ['hello', 'hi']
+//  document.write(arr)
+//  console.log(arr)
+
+
+// var yusraImranBanoQabil = "jee Sahi kaha , yeh pagal hai"; //camelCase
+// var YusraImranBanoQabil; // PascalCase
+// var yusra_imran_bano_qabil //snake_case
+
+// console.log(yusraImranBanoQabil)
+
+//  var num = 13
+// console.log(num)
+
+// var num1 =+ prompt("Num 1 Please")
+// var num2 =+ prompt("Num 2 Please")
+
+//  console.log(Number(num1) + Number(num2))
+//  console.log(num1+num2)
+
+// console.log("1"+"2")
+
+//  console.log(2+6)
+//  console.log(2-6)
+//  console.log(2*6)
+//  console.log(2/6)
+//  console.log(2**6)
+// console.log(((1+(6/7))*(8/9)+6)-(12*5)/3)
+
+// console.log((80/100)*100)
+
+
+// var name = "Usama"
+//  var fname = "Usman"
+//  console.log(name +" "+ fname)
+
+//  var num = 2
+//  console.log(num)
+//  num = num+10
+//  num *=10
+//  num++
+//  ++num
+//  console.log(num)
+
+// Date 6.4.2023
+
+var zakat_percent = 2.5
+var enter_zakat =+ prompt("Enter Your Zakat")
+var result = (enter_zakat*zakat_percent/100)
+alert("Your Zakat is" +" "+ result)
