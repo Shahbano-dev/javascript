91dbe213cc2f783762fea5b584f5d6ccb54dd881
Shahbano Siddique <malikaiza685@gmail.com>
2023-04-07 22:21:02 +0500

    FEAT

--- script/app.js
+++ script/app.js
@@ -1,7 +1,25 @@
 // Date 5.4.2023
 //  alert('hello js')
+// alert("Thanks for input!");
+// alert ( "Thank you!" );
+
 // var user = prompt("Your Name PLease", "Usama")
 // alert(user)
+// var question = "Your species?";
+//  var defaultAnswer = "human";
+//  var spec = prompt(question, defaultAnswer);
+//  alert(spec)
+// var numberOfCats = prompt("How many cats?");
+//  var tooManyCats = numberOfCats + 1;
+//  alert(tooManyCats)
+
+// var name = "Mark";
+// var nationality;
+// nationality = "U.S.";
+// alert(nationality)
+// var floogle = "Mark";
+// alert(floogle)
+
 
 // var arr = ['hello', 'hi']
 //  document.write(arr)
@@ -9,6 +27,11 @@
 
 
 // var yusraImranBanoQabil = "jee Sahi kaha , yeh pagal hai"; //camelCase
+// var lastName = "Shah";
+//  var cityOfOrigin = "New class";
+// var aussieGreeting = "g'Day";
+// alert(cityOfOrigin)
+
 // var YusraImranBanoQabil; // PascalCase
 // var yusra_imran_bano_qabil //snake_case
 
@@ -16,6 +39,14 @@
 
 //  var num = 13
 // console.log(num)
+// var weight = 150;
+// var newNum = weight + 7
+// alert(newNum)
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNumb = originalNum + numToBeAdded;
+// alert(newNumb)
+ 
 
 // var num1 =+ prompt("Num 1 Please")
 // var num2 =+ prompt("Num 2 Please")
@@ -31,13 +62,36 @@
 //  console.log(2/6)
 //  console.log(2**6)
 // console.log(((1+(6/7))*(8/9)+6)-(12*5)/3)
+// var totalCost = 1 + (3 * 4);
+// alert(totalCost)
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// alert(resultOfComputation)
 
 // console.log((80/100)*100)
+// var popularNumber = 3 * 12;
+// var popularNumber = 12 - 24;
+// var popularNumber = 2 + 2;
+// alert(popularNumber)
+// alert(2 + 2);
+// var num = 10;
+//  var popularNumber = num + 200;
+//  alert(popularNumber)
+//  var num = 10;
+//  var anotherNum = 1;
+//  var popularNumber = num + anotherNum;
+//  alert(popularNumber)
+// var whatsLeftOver = 10 % 3;
+// alert(whatsLeftOver)
 
-
-// var name = "Usama"
-//  var fname = "Usman"
+// var name = "shah"
+//  var fname = "siddique"
 //  console.log(name +" "+ fname)
+// var message = "Thanks, ";
+// var userName = "Susan";
+// var banger = "!";
+//  var customMess = message + userName + banger;
+//  alert(customMess);
+// alert("2 plus 2 equals " + 2 + 2);
 
 //  var num = 2
 //  console.log(num)
@@ -46,10 +100,14 @@
 //  num++
 //  ++num
 //  console.log(num)
+// var num = 2;
+//  var newNum = --num;
+// alert(newNum)
 
 // Date 6.4.2023
+// Task 1
 
-var zakat_percent = 2.5
-var enter_zakat =+ prompt("Enter Your Zakat")
-var result = (enter_zakat*zakat_percent/100)
-alert("Your Zakat is" +" "+ result)
+ var zakat_percent = 2.5
+ var enter_zakat =+ prompt("Enter Your Zakat")
+ var result = (enter_zakat*zakat_percent/100)
+ alert("Your Zakat is" +" "+ result)
